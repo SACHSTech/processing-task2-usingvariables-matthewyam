ebb90fdc586eb6e567776a074a3d99182b0db2b3
Matthew Yam <matthew.yam23@ycdsbk12.ca>
2022-02-22 09:32:40 -0500

    add width and height

--- Sketch.java
+++ Sketch.java
@@ -3,34 +3,45 @@ import processing.core.PApplet;
 public class Sketch extends PApplet {
 	
 	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size(500, 500);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(0, 0, 0);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    
+    //The Roof
+    stroke(152, 251, 152);
+    line(width/4, height/2, width/4, height);
+    line((float) (width/1.33), (float)(height/2), (float)(width/1.33), (float)(height));  
+    line(125, 250, 375, 250);
+    
+    //The body of the house
+    stroke(255, 0, 0);
+    fill(0, 0, 0);
+    rect(210, 400, 80, 490);
+    
+    //The Door
+    stroke(21, 244, 238);
+    line(375, 250, 250, 150);
+    line(125, 250, 250, 150);
 
+    //The Doorknob
+    stroke(255, 247, 0);
+    ellipse(280, 455, 10, 10);
+
+    //The Moon
     stroke(255);
-    line(50, 125, 70, 50);  
+    fill(255);
+    ellipse(0, 0, 250, 250);
+    
+    
   }
   
-  // define other methods down here.
+
 }
